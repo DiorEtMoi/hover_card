b69efba0d57d34a6b46ed1f71da994056747cc23
DiorEtMoi <110081209+DiorEtMoi@users.noreply.github.com>
2022-09-08 21:35:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var coursesStr = 'HTML & CSS, JavaScript, ReactJS';
+
+function sum(str) {
+    var arrayStr = str.split(", ");
+    return arrayStr;
+}
+
+var result = sum(coursesStr);
+console.log(result);
